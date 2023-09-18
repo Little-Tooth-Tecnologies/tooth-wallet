3616e0afbd3fc9566797f29be60ff08f6dbbb98d
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-09-18 15:55:31 -0300

    Adicionado Activity de Cadastro
    work in progress

--- app/src/main/java/com/example/littletoothwallet/RegisterActivity.kt
+++ app/src/main/java/com/example/littletoothwallet/RegisterActivity.kt
@@ -3,8 +3,8 @@ package com.example.littletoothwallet
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class CadastroActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle? ) {
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
     }
