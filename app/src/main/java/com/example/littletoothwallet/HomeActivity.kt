62b9ddb29f478199e98b23d20f14e3ba9229fb3b
Mathias Kauer <mathias28kauer@gmail.com>
2023-10-05 22:11:24 -0300

    tela feita home
    falta algumas coisinhas

--- app/src/main/java/com/example/littletoothwallet/HomeActivity.kt
+++ app/src/main/java/com/example/littletoothwallet/HomeActivity.kt
@@ -3,7 +3,7 @@ package com.example.littletoothwallet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
